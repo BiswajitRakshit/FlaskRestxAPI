697c8707794fe55f8600d96dcd97cb527333299c
BiswajitRakshit <biswajitrakshitagcolony@gmail.com>
2021-10-01 02:45:59 +0530

    Final Submit

--- model.py
+++ model.py
@@ -13,9 +13,8 @@ class Operations():
 
     # Fetch by specific Id
     def fetchById(self, ids):
-        for x in self.data:
-            if any(map(lambda m: x[str(m)] == ids, list(x))):
-                return x
+        return next(x for x in self.data if x['id'] == ids)
+            
      
     # Adding Data given by User
     def add(self, dataByUser):
@@ -30,13 +29,16 @@ class Operations():
     
     # Updating Data given by User
     def update(self, dataByUser):
-        for x in self.data:
-            if any(map(lambda m: x[str(m)] == dataByUser['id'], list(x))):
+        
+        next(x for x in self.data if x['id'] == dataByUser['id']).update(dataByUser)
+        
+        # for x in self.data:
+        #     if x['id'] == dataByUser['id']:
                 
-                # Getting Common keys in Data and dataByUser
-                keys = list(set(list(x)).intersection(list(dataByUser)))
-                for k in keys:
-                    x[k] = dataByUser[k]
+        #         # Getting Common keys in Data and dataByUser
+        #         keys = list(set(list(x)).intersection(list(dataByUser)))
+        #         for k in keys:
+        #             x[k] = dataByUser[k]
                     
         #Writting Json Data
         js = json.dumps(self.data)
@@ -47,10 +49,12 @@ class Operations():
  
     # Deleting Data of perticular Id spectified by User
     def delete(self, ids):
-        for i, x in enumerate(self.data):
-            if any(map(lambda m: x[str(m)] == ids, list(x))):
-    
-                    del self.data[i]
+        
+        self.data = [x for x in self.data if not x['id'] == ids]
+        
+        # for i, x in enumerate(self.data):
+        #     if x['id'] == ids:
+        #             del self.data[i]
         
         #Writting Json Data
         js = json.dumps(self.data)
